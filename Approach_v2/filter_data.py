501dbaeed84e7b1fb12e1cd237bc4de7c2ee33ba
HALAXAR <harianshvashisht24155@gmail.com>
2024-10-29 23:05:13 +0530

    updated filter code

--- Approach_v2/filter_data.py
+++ Approach_v2/filter_data.py
@@ -1,6 +1,7 @@
 # For this particular project we will not be taking `disgust` and `fear` emotion into account
 
 import pandas as pd
+import tarfile
 
 def filter_dataframe(df, conditions):
     filtered_df = df.copy()
@@ -10,12 +11,22 @@ def filter_dataframe(df, conditions):
 
     return filtered_df
 
+def extract_file(tar_path, extract_path):
+    try:
+        with tarfile.open(tar_path, 'r') as tar:
+            tar.extractall(path=extract_path)
+        print(f"Successfully extracted to {extract_path}")
+    except Exception as e:
+        print(f"Error extracting tar file: {str(e)}")
+
+
 if __name__ == "__main__":
-    df = pd.read_csv("..//challenges-in-representation-learning-facial-expression-recognition-challenge//icml_face_data.csv")
+    extract_file("..\\challenges-in-representation-learning-facial-expression-recognition-challenge\\fer2013.tar.gz","..\\challenges-in-representation-learning-facial-expression-recognition-challenge\\")
+    df = pd.read_csv("..\\challenges-in-representation-learning-facial-expression-recognition-challenge\\fer2013\\fer2013.csv")
 
     conditions = {'emotion': [1,2]}
     filtered_df = filter_dataframe(df, conditions)
 
 
-    df.to_csv('.//filtered_data.csv', index=False)
+    df.to_csv('.\\filtered_data.csv', index=False)
     print("Data Filtered Successfully.")
